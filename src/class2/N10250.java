64222d61cc0599a4f15ac03531043fba9d75f832
toroy <toroy@DESKTOP-32BBT50>
2023-05-18 02:53:07 +0900

    [230518 김대현] 백준 알고리즘 class2 문제풀이 -10250

---
+++ src/class2/N10250.java
@@ -0,0 +1,27 @@
+package class2;
+
+import java.util.Scanner;
+
+public class N10250 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for(int i = 0; i < t; i++) {
+			
+			int h = sc.nextInt();
+			int w = sc.nextInt();
+			int n = sc.nextInt();
+			
+			if(n % h == 0) {
+				System.out.println((h * 100) + (n / h));
+ 
+			} else {
+				System.out.println(((n % h) * 100) + ((n / h) + 1));
+			}
+		}
+	}
+}
