1313d86bf9b68146606c73eb9687d8907b881dae
toroy <toroy@host.docker.internal>
2023-11-02 14:45:54 +0900

    백준 알고리즘 문제풀이
    class 1
    -27866
    
    class 2
    -2231

---
+++ src/class1/N27866.java
@@ -0,0 +1,17 @@
+package class1;
+
+import java.util.Scanner;
+
+public class N27866 {
+	
+	public static void main(String args[]) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		int i = sc.nextInt();
+		
+		System.out.println(s.charAt(i-1));
+		
+	}
+}
---
+++ src/class2/N2231.java
@@ -0,0 +1,31 @@
+package class2;
+
+import java.util.Scanner;
+
+public class N2231 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int result = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int num = i;
+			int sum = 0;
+			
+			while(num != 0) {
+				sum += num % 10;
+				num /= 10;
+			}
+			
+			if(sum + i == n) {
+				result = i;
+				break;
+			}
+		}
+	
+		System.out.println(result);
+	}
+}
