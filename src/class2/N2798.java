e2290032f315aa3c6beb3abac497608274ad9576
toroy12 <toroy12@naver.com>
2023-12-22 00:07:31 +0900

    백준 알고리즘 문제풀이
    -2798

---
+++ src/class2/N2798.java
@@ -0,0 +1,49 @@
+package class2;
+
+import java.util.Scanner;
+
+public class N2798 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int result = search(arr, n, m);
+		System.out.println(result);
+
+	}
+
+	static int search(int[] arr, int n, int m) {
+
+		int result = 0;
+
+		for (int i = 0; i < n - 2; i++) {
+
+			for (int j = i + 1; j < n - 1; j++) {
+
+				for (int k = j + 1; k < n; k++) {
+
+					int temp = arr[i] + arr[j] + arr[k];
+
+					if (m == temp) {
+						return temp;
+					}
+
+					if (result < temp && temp < m) {
+						result = temp;
+					}
+				}
+			}
+		}
+		return result;
+	}
+}
