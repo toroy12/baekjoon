15035c57e05d1764808eadd05fe0f3335a828c05
toroy12 <toroy12@naver.com>
2024-01-20 14:08:57 +0900

    Merge branch 'master' of https://github.com/toroy12/baekjoon.git

---
+++ src/class2/N1676.java
@@ -0,0 +1,20 @@
+package class2;
+
+import java.io.*;
+
+public class N1676 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		
+		while(n >= 5) {
+			cnt += n / 5;
+			n /= 5;
+		}
+		System.out.println(cnt);
+	}
+}
