b568f15a655f78a2e554e46c80785f7244238410
toroy12 <toroy12@naver.com>
2024-01-20 13:30:24 +0900

    백준 알고리즘 문제풀이
    -2869

--- src/class2/N2869.java
+++ src/class2/N2869.java
@@ -1,8 +1,6 @@
 package class2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class N2869 {
