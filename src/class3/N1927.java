ca59cbb302e7d8c040d351e7cef53f121e30b67d
toroy12 <toroy12@naver.com>
2024-04-17 22:06:37 +0900

    백준 알고리즘 문제풀이
    -1927

---
+++ src/class3/N1927.java
@@ -0,0 +1,25 @@
+package class3;
+
+import java.util.*;
+import java.io.*;
+
+public class N1927 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> queue = new PriorityQueue<>();
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			int val = Integer.parseInt(br.readLine());
+			
+			if(val == 0) {
+				if(queue.isEmpty())
+					System.out.println("0");
+				else
+					System.out.println(queue.poll());
+			}
+			else
+				queue.add(val);
+		}
+	}
+}
